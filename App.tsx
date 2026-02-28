21be3ba844ceb7ada86bf0e07d53f32ea4cd8f8d
kunalaveerakalpana-hub <kunalaveerakalpana@gmail.com>
2026-02-28 21:52:04 +0530

    Add files via upload

---
+++ App.tsx
@@ -0,0 +1,21 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+import Home from './pages/Home';
+import Services from './pages/Services';
+import Contact from './pages/Contact';
+
+export default function App() {
+  return (
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/services" element={<Services />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/Home" element={<Home/>} />
+      </Routes>
+      <Footer />
+    </Router>
+  );
+}
